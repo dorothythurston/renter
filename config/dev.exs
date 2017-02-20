512278b34c9e19d4c79cd892e19f318bd8ae7e5d
Dorothy Thurston <dorothy.thur@gmail.com>
2017-02-20 12:15:44 -0500

    remove credentials for test and dev env dbs

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :renter, Renter.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "renter_dev",
   hostname: "localhost",
   pool_size: 10
